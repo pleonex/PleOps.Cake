e30df6b5979a3c0a2476911c9bd20d26cd17c077
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-10-06 07:32:43 +0200

    :sparkles: Add test projects and configure SourceLink

--- build/Program.cs
+++ build/Program.cs
@@ -1,32 +1,47 @@
 ﻿using Cake.Core;
+using Cake.Core.Diagnostics;
 using Cake.Frosting;
 using PleOps.Cake.Frosting;
 
 return new CakeHost()
     .AddAssembly(typeof(BuildContext).Assembly)
-    .UseContext<BuildContext>()
+    .UseContext<MyCustomContext>()
     .UseLifetime<BuildLifetime>()
     .Run(args);
 
-public sealed class BuildLifetime : FrostingLifetime<BuildContext>
+public sealed class MyCustomContext : BuildContext
 {
-    public override void Setup(BuildContext context, ISetupContext info)
+    public MyCustomContext(ICakeContext context)
+        : base(context)
+    {
+        CustomSetting = "DefaultValue";
+    }
+
+    public string CustomSetting { get; set; }
+}
+
+public sealed class BuildLifetime : FrostingLifetime<MyCustomContext>
+{
+    public override void Setup(MyCustomContext context, ISetupContext info)
     {
         // HERE: you can set default values overridable by command-line
+        context.CustomSetting = "LifetimeValue";
 
         // Fill context from command line arguments.
         context.ReadArguments();
+        context.IfArgIsPresent("custom-setting", x => context.CustomSetting = x);
 
         // Set the projects version from GitVersion tool.
         context.SetGitVersion();
 
         // HERE: you can force values non-overridables.
+        context.CustomSetting = "ForcedValue";
 
         // Print the build info to use.
         context.Print();
     }
 
-    public override void Teardown(BuildContext context, ITeardownContext info)
+    public override void Teardown(MyCustomContext context, ITeardownContext info)
     {
     }
 }
@@ -36,4 +51,13 @@ public sealed class BuildLifetime : FrostingLifetime<BuildContext>
 [IsDependentOn(typeof(PleOps.Cake.Frosting.Dotnet.BuildTask))]
 public sealed class DefaultTask : FrostingTask
 {
+}
+
+[TaskName("CustomTask")]
+public sealed class CustomTask : FrostingTask<MyCustomContext>
+{
+    public override void Run(MyCustomContext context)
+    {
+        context.Log.Information("Custom setting: {0}", context.CustomSetting);
+    }
 }
