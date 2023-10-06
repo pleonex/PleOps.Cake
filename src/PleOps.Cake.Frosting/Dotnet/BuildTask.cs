c59e75997b0c1018e1304a1689f3cb5b12c024bf
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-10-06 14:06:07 +0200

    :sparkles: Add warning mode for build and hide summary

--- src/PleOps.Cake.Frosting/Dotnet/BuildTask.cs
+++ src/PleOps.Cake.Frosting/Dotnet/BuildTask.cs
@@ -15,12 +15,17 @@ public class BuildTask : FrostingTask<BuildContext>
         logPath = Path.Combine(context.TemporaryPath, logPath);
         string binaryLoggerAssembly = typeof(Microsoft.Build.Logging.StructuredLogger.BinaryLog).Assembly.Location;
 
+        var warningMode = context.WarningsAsErrors
+            ? MSBuildTreatAllWarningsAs.Error
+            : MSBuildTreatAllWarningsAs.Default;
+
         var settings = new DotNetBuildSettings {
             Verbosity = context.CSharpContext.ToolingVerbosity,
             Configuration = context.CSharpContext.Configuration,
             NoRestore = true,
             MSBuildSettings = new DotNetMSBuildSettings()
                 .WithProperty("Platform", context.CSharpContext.Platform)
+                .TreatAllWarningsAs(warningMode)
                 .SetVersion(context.Version)
                 // Use the binary logger from the addin.
                 // MSBuild may have an incompatible version.
@@ -28,11 +33,24 @@ public class BuildTask : FrostingTask<BuildContext>
                     "BinaryLogger," + binaryLoggerAssembly,
                     "",
                     logPath)
-                // For the error parser of VS Code
+                // This setting improve the experience with VS Code
+                .HideDetailedSummary()
                 .WithProperty("GenerateFullPaths", "true"),
         };
         context.DotNetBuild(context.CSharpContext.SolutionPath, settings);
 
         context.IssuesContext.Parameters.InputFiles.AddMsBuildBinaryLogFile(logPath);
+    }
+}
+
+internal static class MSBuildSettingExtensions
+{
+    public static DotNetMSBuildSettings HideDetailedSummary(this DotNetMSBuildSettings settings)
+    {
+        if (settings == null)
+            throw new ArgumentNullException(nameof(settings));
+
+        settings.DetailedSummary = false;
+        return settings;
     }
 }
